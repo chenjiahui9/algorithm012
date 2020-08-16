b723fd658e68dbd72bfe191dc3f610ab43f93a6e
chenjiahui9 <chenjiahui_job@163.com>
2020-08-16 22:29:14 +0800

    homework 06

--- Week_06/README.md
+++ Week_06/README.md
@@ -1,1 +1,45 @@
 学习笔记
+
+递归模板
+public void recur(int level,int param) {
+
+	if level > max {//递归终止条件 1
+		return
+	}
+	
+	process(level,param)//处理当前层逻辑 2
+	
+	recur(level,param)//递归到下一层 3
+	
+	//有需要的话恢复当前层状态	4
+}
+
+分治代码模板
+def divide_conquer(problem,param1,param2):
+	
+	# recursion terminator(递归终止条件)
+	if problem is None:
+		print_result
+		return
+	
+	# prepare data(拆分子问题)
+	data = prepare_data(problem)
+	subproblems = split_problem(problem,data)
+	
+	# conquer subproblems （调子问题的递归函数）
+	subresult1 = self.divide_conquer(subproblems[0],p1,...)
+	subresult2 = self.divide_conquer(subproblems[1],p2,...)
+	subresult3 = self.divide_conquer(subproblems[2],p3,...)
+	...
+	
+	#process and generate the final result （合并结果）
+	result = process_result(subresult1,subresult2,subresult3,...)
+	#reveret the current level states 恢复当前层状态
+	
+
+
+1.动态规划的实现及关键点
+1） 人肉递归低效，很累
+2） 找到最近最简的方法，将其拆解成可重复解决的问题
+3） 数学归纳法思维（抵制人肉递归的诱惑）
+	
---
+++ Week_06/findWords.go
@@ -0,0 +1,59 @@
+//定义前缀树
+type TrieNode struct {
+    word string
+    children [26]*TrieNode
+}
+
+func findWords(board [][]byte, words []string) []string {
+    //申明前缀树根
+    root := &TrieNode{}
+    //构造前缀树
+    //遍历words 单词列表
+    for _,word := range words {
+        //将root赋给node
+        node := root
+        //遍历单个单词字母
+        for _,w := range word {
+            if node.children[w-'a'] == nil {
+                node.children[w-'a'] = &TrieNode{}
+            }
+            node = node.children[w-'a']
+        }
+        node.word = word
+    }
+    
+    result := make([]string,0)
+    for i:=0; i<len(board); i++{
+        for j:=0; j<len(board[0]); j++{
+            dfs(i,j,board,root,&result)
+        }
+    }
+    return result
+}
+
+//深度优先遍历
+func dfs(i int,j int,broad [][]byte,node *TrieNode,result *[]string){
+    //越界处理
+    if i < 0 || j < 0 || i==len(broad) || j==len(broad[0]) {
+        return
+    }
+    //获取值
+    c := broad[i][j]
+    if c == '#' || node.children[c-'a'] == nil {//访问过或为空
+        return
+    }
+
+    node = node.children[c-'a']
+    
+    if node.word != "" {
+        *result = append(*result,node.word)
+        node.word = ""
+    }
+
+    broad[i][j] = '#'
+    dfs(i+1,j,broad,node,result)
+    dfs(i-1,j,broad,node,result)
+    dfs(i,j+1,broad,node,result)
+    dfs(i,j-1,broad,node,result)
+    broad[i][j] = c
+}
